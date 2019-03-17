9ee49bdfae7c1a889d3c000bf26b02d0ef29b1bd
FIdelis Msacky <fidelismsacky@yahoo.com>
2019-03-17 19:03:54 -0400

    Updated: - expandable SERVICE text on home page

--- app/pages/home_page/home_page.jsx
+++ app/pages/home_page/home_page.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './home_page.css';
-import ReactDOM from "react-dom";
-import {Services} from "Pages/services/services";
+import ReactDOM from 'react-dom';
+import {Services} from 'Pages/services/services';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPlus} from '@fortawesome/free-solid-svg-icons';
+import 'CSS/font-awesome.min.css';
 
 export class HomePage extends React.Component {
     constructor(props) {
@@ -22,32 +25,45 @@ export class HomePage extends React.Component {
                         <h2>Our Core Values</h2>
                     </header>
                     <div className="container">
-                        <p><span className="bold-leader">S</span>ervice Excellence: We promise and deliver quality
+                        <div className="mnemonic"><p className="mnemonic-text"><span
+                            className="bold-leader">S</span>ervice Excellence: We promise and deliver quality
                             service to meet and exceed
-                            the needs of patients and clients.</p>
-                        <p><span className="bold-leader">E</span>nterprising Work Ethic: We promote personal and
+                            the needs of patients and clients.</p></div>
+                        <div className="mnemonic"><p className="mnemonic-text"><span
+                            className="bold-leader">E</span>nterprising Work Ethic: We promote personal and
                             organizational progress
-                            through innovating and incorporating leading practices.</p>
-                        <p><span className="bold-leader">R</span>espect and Compassion: We pledge to treat every Vision
+                            through innovating and incorporating leading practices.</p></div>
+                        <div className="mnemonic"><p className="mnemonic-text"><span
+                            className="bold-leader">R</span>espect and Compassion: We pledge to treat every Vision
                             team member, patient,
                             and client with the utmost respect and dignity. We also recognize that Vision is in the
                             business of improving lives, and as a result, we expect that every team member
                             exhibits heartfelt compassion and empathy in every interaction, every day.</p>
-                        <p><span className="bold-leader">V</span>igilant focus on today and tomorrow:
+                            <span className="icon" id="icon-1"><FontAwesomeIcon icon={faPlus} size="xs"/></span>
+                        </div>
+                        <div className="mnemonic"><p className="mnemonic-text"><span
+                            className="bold-leader">V</span>igilant focus on today and tomorrow:
                             We embrace change and utilize the best people and processes to improve ourselves
-                            and our services</p>
-                        <p><span className="bold-leader">I</span>ndividualized quality: Care that meets the total needs
+                            and our services</p></div>
+                        <div className="mnemonic"><p className="mnemonic-text"><span
+                            className="bold-leader">I</span>ndividualized quality: Care that meets the total needs
                             of the patient and family
                             whose care can be safely and effectively managed in the home care environment
                             without regards to race, sex, religion, or age.</p>
-                        <p><span className="bold-leader">C</span>ontinuous Growth and Improvement: We are focused on
+                            <span className="icon" id="icon-2"><FontAwesomeIcon icon={faPlus} size="xs"/></span></div>
+                        <div className="mnemonic"><p className="mnemonic-text"><span
+                            className="bold-leader">C</span>ontinuous Growth and Improvement: We are focused on
                             today and tomorrow.
                             We embrace change and utilize the best people and processes to improve ourselves
-                            and our services.</p>
-                        <p><span className="bold-leader">E</span>thical Behavior and Integrity: We are dedicated to the
+                            and our services.</p><span className="icon" id="icon-3"><FontAwesomeIcon icon={faPlus} size="xs"/></span>
+                        </div>
+                        <div className="mnemonic"><p className="mnemonic-text"><span
+                            className="bold-leader">E</span>thical Behavior and Integrity: We are dedicated to the
                             highest level of
                             compliance and personal integrity. We believe in doing the right thing and expect this
                             behavior from others.</p>
+                            <span className="icon" id="icon-4">
+                                <FontAwesomeIcon icon={faPlus} size="xs"/></span></div>
                     </div>
                 </div>
 
